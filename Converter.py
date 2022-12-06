5acf129592f168d2c2dc0bef16ecca36376f3ee7
Jake Rhee <jakerhee321@gmail.com>
2022-12-07 07:59:42 +0900

    Window

--- Converter.py
+++ Converter.py
@@ -3,7 +3,16 @@ import PySimpleGUI as sg
 layout = [
     [sg.Text('Text'),sg.Spin(['item 1', 'item 2'])],
     [sg.Button('Button')],
-    [sg.Input()]
+    [sg.Input()],
+    [sg.Text('Test'), sg.Button('Test Button')]
 ]
 
-sg.Window('Converter', layout).read()
+window = sg.Window('Converter', layout).read()
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
