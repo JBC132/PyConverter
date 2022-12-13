6cea26327be21e4a4189584824f3741b3f5dae22
Jake Rhee <jakerhee321@gmail.com>
2022-12-13 10:39:01 +0900

    Finishing code

--- Converter.py
+++ Converter.py
@@ -21,10 +21,19 @@ while True:
         input_value = values['-INPUT-']
         if input_value.isnumeric():
             match values['-UNITS-']:
+                # match statements only available on Python 3.10
                 case 'km to mile':
-                    output = round(float(input_value)*0.6214)
+                    output = round(float(input_value)*0.6214,2)
                     output_string = f'{input_value} km are {output} miles.'
+                case 'kg to pound':
+                    output = round(float(input_value)*2.20462,2)
+                    output_string = f'{input_value} kg are {output} pounds.'
+                case 'sec to min':
+                    output = round(float(input_value)/60,2)
+                    output_string = f'{input_value} second are {output} minute.'
             
             window['-OUTPUT-'].update(output_string)
+        else:
+            window['-OUTPUT-'].update('Please enter a number')
 
 window.close()
